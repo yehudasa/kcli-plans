304f59d8e2f5124c8928e0c06d07e7a9b6b5d826
Yehuda Sadeh <yehuda@redhat.com>
2021-06-09 13:37:28 -0700

    wip

--- ceph/bootstrap-cluster.sh
+++ ceph/bootstrap-cluster.sh
@@ -18,5 +18,5 @@ fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 {% for number in range(1, nodes) %}
   ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}.{{ domain }}
   cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch host add {{ prefix }}-node-0{{ number }}.{{ domain }}
-#{% endfor %}
+{% endfor %}
 cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch apply osd --all-available-devices
